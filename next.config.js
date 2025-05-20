f6a20cb9b4a5a4c27d234e3a798ec0e0313c8780
LaggyJet <jskrillex6@gmail.com>
2025-05-20 02:04:19 -0400

    More config changes pt10

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
-const isGithubPages = process.env.DEPLOY_ENV === 'GH_PAGES';
-const basePath = isGithubPages ? '/DigitalPortfolioWebsite' : '';
+const isGithubPages = process.env.DEPLOY_ENV === 'GH_PAGES'
+const basePath = isGithubPages ? '/DigitalPortfolioWebsite' : ''
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
-  basePath: basePath,
-  assetPrefix: basePath,
+  basePath,
+  assetPrefix: basePath + '/',
   images: {
     unoptimized: true,
   },
   env: {
     NEXT_PUBLIC_BASE_PATH: basePath,
   },
-};
+}
+
+module.exports = nextConfig
