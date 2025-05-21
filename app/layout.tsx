8b3aa694b296fe10117133c8e00d4649afef6b04
LaggyJet <jskrillex6@gmail.com>
2025-05-21 05:01:55 -0400

    Update Website title getting set based off page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,30 @@
+'use client';
+
 import './globals.css';
 import Header from './Header/Header';
-import ButtonAnimationDelay from './Components/ButtonAnimationDelay';
+import { ButtonAnimationDelay, formatTitleFromPath } from './Components/HelperFunctions';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+    const pathname = usePathname();
+    const [title, setTitle] = useState('JF Portfolio');
+    useEffect(() => {
+        setTitle(`${formatTitleFromPath(pathname)} - JF Portfolio`);
+    }, [pathname]);
+    useEffect(() => {
+        document.title = title;
+    }, [title]);
     return (
         <html lang="en" style={{ backgroundColor: "#151517" }}>
-            <body style={{ backgroundColor: "#151517" }}>
-                <Header />
-                <div className="background">
-                    {children}
-                </div>
-                <ButtonAnimationDelay />
-            </body>
+        <head>
+            <title>{title}</title>
+        </head>
+        <body style={{ backgroundColor: "#151517" }}>
+            <Header />
+            <div className="background">{children}</div>
+            <ButtonAnimationDelay />
+        </body>
         </html>
     );
 }
