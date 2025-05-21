c6b7ee45b47a67299b0f25cc2bde0f7493bb1e75
LaggyJet <jskrillex6@gmail.com>
2025-05-21 04:26:37 -0400

    Remove flashbang effect when loading page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,16 @@ import { ReactNode } from 'react';
 import Header from './Header/Header';
 import ButtonAnimationDelay from './Components/ButtonAnimationDelay';
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
-        <body>
-            <Header />
-            <div className="background">
-            {children}
-            </div>
-            <ButtonAnimationDelay />
-        </body>
+        <html lang="en" style={{ backgroundColor: "#151517" }}>
+            <body style={{ backgroundColor: "#151517" }}>
+                <Header />
+                <div className="background">
+                    {children}
+                </div>
+                <ButtonAnimationDelay />
+            </body>
         </html>
     );
 }
