ca03214fda2cbc521ccd79c3bdf624d954913614
LaggyJet <jskrillex6@gmail.com>
2025-05-22 00:47:48 -0400

    Updated Resume Page

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -3,8 +3,12 @@
 export default function ContactPage() {
     return (
         <div className="container">
-            <h1 className="title">My Resume</h1>
-            <embed src="/JF-Resume.pdf" type="application/pdf" width="100%" height="600px" />
+            <h1>
+                <a className="button" href="/JF-Resume.pdf" download>
+                    My Resume
+                </a>
+            </h1>
+            <embed src="/JF-Resume.pdf" type="application/pdf" width="100%" height="100%" />
         </div>
     );
 }
