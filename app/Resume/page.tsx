8c43301158cc8e483a5cd90afaf1d9ffcd0589af
LaggyJet <jskrillex6@gmail.com>
2025-05-22 00:49:23 -0400

    Updated Resume Page

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -5,7 +5,7 @@ export default function ContactPage() {
         <div className="container">
             <h1>
                 <a className="button" href="/JF-Resume.pdf" download>
-                    My Resume
+                    Download Resume
                 </a>
             </h1>
             <embed src="/JF-Resume.pdf" type="application/pdf" width="100%" height="100%" />
